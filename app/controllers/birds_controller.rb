f9a39aa01a454f1e16756dd0815eee2bc0e11fd0
George Baroi <baroi.george@gmail.com>
2022-09-28 15:47:18 -0400

    commit tree a3b35fa8b9d75496d1d0f5acfbdcca18676bd1d5

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordNotFound, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -10,7 +11,11 @@ class BirdsController < ApplicationController
   # POST /birds
   def create
     bird = Bird.create(bird_params)
+    if bird.valid?
     render json: bird, status: :created
+    else
+      render json: { errors: bird.errors}, status: :unprocessable_entity
+    end
   end
 
   # GET /birds/:id
@@ -45,6 +50,9 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response
   end
 
 end
